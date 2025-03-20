17585b43df24a2ff9e780ce71ac89c71ca389d77
Jesse Reid <jreid@Jesses-MacBook-Pro.local>
2025-03-20 10:21:31 -0400

    test java code

--- FarenheitToCelsius.java
+++ FarenheitToCelsius.java
@@ -4,8 +4,8 @@ public class FarenheitToCelsius {
         int sundayFarenheit;
         saturdayFarenheit = 78;
         sundayFarenheit = 81;
-        double saturdayCelsius = (5.0/9) * (saturdayFarenheit);
-        double sundayCelsius = (5.0/9) * (sundayFarenheit);
+        double saturdayCelsius = (5.0/9) * (saturdayFarenheit - 32);
+        double sundayCelsius = (5.0/9) * (sundayFarenheit - 32);
         System.out.println("Weekend Averages");
         System.out.println("Saturday :" + saturdayCelsius);
         System.out.println("Sunday :" + sundayCelsius);
